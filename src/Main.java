33383af410a3d203e03ba69ab0aca46f0a45da6c
MikhailSidelev <mikhailsidelev@gmail.com>
2023-08-02 18:29:25 -0400

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int num=4;
-        int num1=6;
-        System.out.println(num = num1);
+        int a=4;
+        int b=6;
+        System.out.println(a+b);
         }
     }
-}
